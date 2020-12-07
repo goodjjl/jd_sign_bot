f52e541dd0b2f869118f2191f2a00e9673e73157
Guosen Wang <do_not@me.com>
2020-12-07 20:56:43 +0800

    Update notify logic

--- app.js
+++ app.js
@@ -63,12 +63,11 @@ async function start() {
     if (fs.existsSync(path)) {
       content = fs.readFileSync(path, "utf8");
     }
-    let t = content.match(/【签到概览】:((.|\n)*)【签到总计】/)
+    let t = content.match(/【签到概览】:  ((.|\n)*)【签到奖励】/)
     let res = t ? t[1].replace(/\n/,'') : '失败'
-    let t2 = content.match(/【签到总计】:((.|\n)*)【账号总计】/)
+    let t2 = content.match(/【签到奖励】:  ((.|\n)*)【账号总计】/)
     let res2 = t2 ? t2[1].replace(/\n/,'') : '总计0'
 
-    
     await sendNotify("" + ` ${res2} ` + ` ${res} ` + new Date().toLocaleDateString(), content);
   }
 }
